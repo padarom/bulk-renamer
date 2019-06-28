1e8fd996c41e309ed583645cc5bb14026dd560b5
Christopher Mühl <christopher@padarom.io>
2019-06-29 00:53:51 +0200

    Add bulma and stylus

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import 'bulma/css/bulma.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
